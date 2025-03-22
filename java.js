e51d3f78c9bb1e9ae46286068979d193a1d7514f
zeke016 <vasconceldaiane@gmail.com>
2025-03-22 14:28:34 -0300

    first commit

---
+++ java.js
@@ -0,0 +1,4 @@
+document.getElementById('acceptButton').addEventListener('click', function() {
+    const responseMessage = document.getElementById('response');
+    responseMessage.textContent = 'Obrigado por aceitar o convite! Estamos ansiosos para sua presença.';
+});
